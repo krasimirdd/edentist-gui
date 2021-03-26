696a6c9a5e390763ebf865b9846a91addc425e81
Krasimir Dimitrov <kdduni98@gmail.com>
2021-03-26 18:20:14 +0200

    redeploy 1.0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import {clientId, domain} from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+  }
 };
